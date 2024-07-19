09f6d0c2ec0483d7c28bc9f3a6ea3ae87208deef
김연지 <yj009977@hanmail.net>
2024-07-19 17:06:41 +0900

    feat(FindTeam) : 팀 모집 작성 페이지 구현, 태그 기능 구현

---
+++ frontend/src/components/SearchBar.tsx
@@ -0,0 +1,53 @@
+import React from "react"
+
+interface SearchBarProps {
+    value : string;
+    onChange : (event : React.ChangeEvent<HTMLInputElement>) => void 
+}
+
+export function SearchBar({value, onChange} : SearchBarProps){
+    return (
+        <>
+        <input
+        type="search"
+        placeholder="검색하기"
+        value={value}
+        onChange={onChange}
+        />
+        </>
+    )
+}
+
+
+interface Country {
+    code: string;
+    en: string;
+    ko: string;
+}
+
+interface SearchResultsProps {
+    countries: Country[];
+    searching: boolean;
+}
+
+
+export function SearchResults({ countries, searching }: SearchResultsProps) {
+    return (
+        <article aria-busy={searching}>
+            {searching ? (
+                "잠시만 기다려주세요. 국가를 검색하고 있습니다."
+            ) : (
+                <>
+                    <header>총 {countries.length}개의 국가가 검색되었습니다.</header>
+                    <ul>
+                        {countries.map(({ code, en, ko }) => (
+                            <li key={code}>
+                                {ko} ({en})
+                            </li>
+                        ))}
+                    </ul>
+                </>
+            )}
+        </article>
+    );
+}
--- frontend/src/pages/FindTeamPage.tsx
+++ frontend/src/pages/FindTeamPage.tsx
@@ -1,10 +1,38 @@
+import React, {useState} from "react"
+
 import styled from "styled-components"
 import { Tag } from "../components/Tag"
+import { SearchBar } from "../components/SearchBar"
+import Button from "../components/Button"
+import BasicCard from "../components/Card"
+import FindTeam from "../components/FindTeam"
 
 
-const FindTeamPage = () => {
+// interface CardProps {
+//     tags :
+// }
+
+const FindTeamPage:React.FC = () => {
+    const [visible,setVisible] = useState(false)
+
     return (
+        <>
         <Tag/>
+        <SearchBar/>
+        <Button/>
+        <button>글 작성</button>
+        <h2>팀찾기</h2>
+        <BasicCard 
+        />
+        <button
+        onClick={()=>{
+            setVisible(!visible)
+        }}
+        >
+            {visible ? "작성하기" : "보이기"}
+        </button>
+        {visible && <FindTeam/>}
+        </>
     )
 
 
