db6c636e749f2c68b7f91fa3335d1c670ef045bf
Lee-JounghHyun <dnlwnd24@gmail.com>
2024-07-26 05:30:14 +0900

    feat(Entity): Invitation 테이블 Entity 생성

--- backend/storyboat/src/main/java/com/ssafy/storyboat/domain/studio/entity/Invitation.java
+++ backend/storyboat/src/main/java/com/ssafy/storyboat/domain/studio/entity/Invitation.java
@@ -1,9 +1,6 @@
 package com.ssafy.storyboat.domain.studio.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Invitation {
@@ -11,4 +8,12 @@ public class Invitation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "studio_id")
+    private Studio studio;
+
+    private String title;
+
+    private String description;
 }
