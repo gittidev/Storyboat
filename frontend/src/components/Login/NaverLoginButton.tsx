394706db220b613b12a7f81b0bf8b9729f3c7d60
김연지 <yj009977@hanmail.net>
2024-07-25 14:40:10 +0900

    feat(login) : 프론트 로그인 기능 구현

--- frontend/src/components/Login/NaverLoginButton.tsx
+++ frontend/src/components/Login/NaverLoginButton.tsx
@@ -1,20 +1,34 @@
 import React from 'react';
-import { getNaverAuthToken } from '../../apis/auth'
+// import { getNaverAuthToken } from '../../apis/auth'
+
+// const NaverLoginButton: React.FC = () => {
+//   const handleLogin = async () => {
+//     try {
+//       const token = await getNaverAuthToken();
+//       console.log('Received token:', token);
+//       // 여기서 토큰을 저장하거나 다른 작업을 수행
+//     } catch (error) {
+//       console.error('Failed to login with Naver:', error);
+//     }
+//   };
+
+//   return (
+//     <button className='naver-btn' onClick={handleLogin}>
+//       Sign in with Naver
+//     </button>
+//   );
+// };
+
+// export default NaverLoginButton;
 
 const NaverLoginButton: React.FC = () => {
-  const handleLogin = async () => {
-    try {
-      const token = await getNaverAuthToken();
-      console.log('Received token:', token);
-      // 여기서 토큰을 저장하거나 다른 작업을 수행
-    } catch (error) {
-      console.error('Failed to login with Naver:', error);
-    }
+  const handleLogin = () => {
+    window.location.href = 'http://localhost:8080/oauth2/authorization/naver';
   };
 
   return (
-    <button className='naver-btn' onClick={handleLogin}>
-      Sign in with Naver
+    <button onClick={handleLogin}>
+        네이버로 로그인
     </button>
   );
 };
