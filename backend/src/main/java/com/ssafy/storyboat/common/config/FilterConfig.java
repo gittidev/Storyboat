fa0a65930cae06ad6a540759f60aca1d9a212839
이중현 <dnlwnd24@daum.net>
2024-07-31 13:35:00 +0900

    test: 필터 빈등록 헤제

--- backend/src/main/java/com/ssafy/storyboat/common/config/FilterConfig.java
+++ backend/src/main/java/com/ssafy/storyboat/common/config/FilterConfig.java
@@ -6,14 +6,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 import org.springframework.core.Ordered;
 
-@Configuration
+//@Configuration
 public class FilterConfig {
 
-    @Bean
+    //@Bean
     public FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
         FilterRegistrationBean<ForwardedHeaderFilter> bean = new FilterRegistrationBean<>();
-        //bean.setFilter(new ForwardedHeaderFilter());
-        //bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        bean.setFilter(new ForwardedHeaderFilter());
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
         return bean;
     }
 }
