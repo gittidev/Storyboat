5f3d17ed957ff4ac818fa1a3308ba33aa2484eaf
Lee-JoungHyun <doublehyun98@gmail.com>
2024-08-06 10:21:10 +0900

    feat:태그 목록조회, 태그 상세 조회 개발

--- backend/src/main/java/com/ssafy/storyboat/domain/tag/application/TagService.java
+++ backend/src/main/java/com/ssafy/storyboat/domain/tag/application/TagService.java
@@ -1,5 +1,6 @@
 package com.ssafy.storyboat.domain.tag.application;
 
+import com.ssafy.storyboat.common.exception.ResourceNotFoundException;
 import com.ssafy.storyboat.domain.tag.entity.Tag;
 import com.ssafy.storyboat.domain.tag.repository.TagRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,5 +19,16 @@ public class TagService {
     @Transactional(readOnly = true)
     public List<Tag> findTagByProfileId(Long profileId) {
         return tagRepository.findTagsByProfileId(profileId);
+    }
+
+    @Transactional(readOnly = true)
+    public Tag findTagById(Long id) {
+        return tagRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("해당 태그 존재하지 않음"));
+    }
+
+    @Transactional(readOnly = true)
+    public List<Tag> findAllTags() {
+        return tagRepository.findAll();
     }
 }
