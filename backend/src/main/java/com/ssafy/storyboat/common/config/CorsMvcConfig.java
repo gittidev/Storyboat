2de306df1ab78d43736cd1b3ad36e49aefaeea47
kimsion <siokim001@gmail.com>
2024-08-06 17:24:46 +0900

    fix: cors error 해결

--- backend/src/main/java/com/ssafy/storyboat/common/config/CorsMvcConfig.java
+++ backend/src/main/java/com/ssafy/storyboat/common/config/CorsMvcConfig.java
@@ -9,7 +9,7 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:8080", "https://localhost:5173")
+            .allowedOrigins("http://localhost:5173", "http://localhost:8080", "https://localhost:5173", "https://i11c107.p.ssafy.io")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization", "x-xsrf-token", "Access-Control-Allow-Headers", "Origin", "Accept", "X-Requested-With", "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers")
--- backend/src/main/java/com/ssafy/storyboat/common/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/storyboat/common/config/SecurityConfig.java
@@ -93,6 +93,9 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.addAllowedOrigin("http://localhost:5173");
+        configuration.addAllowedOrigin("http://localhost:8080");
+        configuration.addAllowedOrigin("https://localhost:5173");
+        configuration.addAllowedOrigin("https://i11c107.p.ssafy.io");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
