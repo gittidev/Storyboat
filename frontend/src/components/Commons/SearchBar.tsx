d16876239fc10846e9c0d7d5db1172cc16487a01
김연지 <yj009977@hanmail.net>
2024-08-15 14:29:12 +0900

    fix : 팀찾기 검색창 수정

--- frontend/src/components/Commons/SearchBar.tsx
+++ frontend/src/components/Commons/SearchBar.tsx
@@ -7,50 +7,65 @@ interface SearchBarProps {
 
 export const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
     const [keyword, setKeyword] = useState("");
-    const [category, setCategory] = useState("title"); // 기본 카테고리는 title로 설정
+    const [category, setCategory] = useState("title");
+    const [isFocused, setIsFocused] = useState(false);
 
     const handleSearch = () => {
         onSearch(category, keyword);
     };
 
+    const handleFocus = () => {
+        setIsFocused(true);
+    };
+
+    const handleBlur = () => {
+        if (!keyword) {
+            setIsFocused(false);
+        }
+    };
+
+    const handleKeyPress = (event: React.KeyboardEvent<HTMLDivElement>) => {
+        if (event.key === 'Enter') {
+            handleSearch();
+        }
+    };
+
     return (
         <Box sx={{ display: "flex", alignItems: "center", marginBottom: "20px" }}>
-            
-            <div className="SearchBarbody " style={{ marginTop: '15px', marginBottom: '15px' }}>
-
-           
             <FormControl 
                 sx={{ 
                     minWidth: 120, 
                     marginRight: "10px", 
-                    height: "40px" // 버튼과 같은 높이로 설정
+                    height: "40px"
                 }}>
                 <InputLabel>카테고리</InputLabel>
                 <Select
                     value={category}
                     onChange={(e) => setCategory(e.target.value as string)}
                     label="카테고리"
-                    sx={{ height: "100%" }} // FormControl의 높이에 맞추기
+                    sx={{ height: "100%" }}
                 >
                     <MenuItem value="title">제목</MenuItem>
-                    <MenuItem value="category">카테고리</MenuItem>
+                    <MenuItem value="category">태그</MenuItem>
                 </Select>
             </FormControl>
-            </div>
+
             <TextField
                 variant="outlined"
-                label="검색어 입력"
+                label={isFocused ? "" : "검색어 입력"}
                 value={keyword}
+                onFocus={handleFocus}
+                onBlur={handleBlur}
                 onChange={(e) => setKeyword(e.target.value)}
+                onKeyPress={handleKeyPress} 
                 sx={{ 
-              
                     marginRight: "10px", 
                     height: "40px",
-                    "& .MuiInputBase-root": { height: "100%" }, // TextField 높이 맞추기
+                    "& .MuiInputBase-root": { height: "100%" },
                     "& .MuiInputLabel-root": { 
-                        top: "50%", // 레이블을 중앙에 맞추기
-                        transform: "translateY(-50%)", // 중앙 정렬을 위한 변환
-                        zIndex: 1, // 레이블이 입력 필드 위에 보이도록 설정
+                        top: "50%", 
+                        transform: "translateY(-50%)", 
+                        zIndex: 1,
                         left: "12px",
                     }
                 }}
@@ -59,7 +74,7 @@ export const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
                 variant="outlined" 
                 color="primary" 
                 onClick={handleSearch}
-                sx={{ height: "40px" }} // 버튼 높이 설정
+                sx={{ height: "40px" }}
             >
                 검색
             </Button>
